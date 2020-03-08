d591a3e10696f67bf99153bdaf667dd1c0d627b4
Mahendra <mahendra.jeto.kumar40@gmail.com>
2020-03-08 19:02:23 +0530

    update on 08_03_2020

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(function() {
 
+	/*	MOBILE MENU	*/
+	//$('.header-container').append('<div class="menu-trigger"><span></span></div>');
+	$('.menu-trigger').click(function() {
+		$(this).toggleClass('active');
+		$('body').toggleClass('show-toggle-nav');
+		//$('.site-mainMenu-wrapper').slideToggle(250).toggleClass('show');
+		$('.site-mainMenu-wrapper').toggleClass('show');
+	});
+
+
+	/*	SCROLL HEADER	*/
 	$(window).scroll(function() {
 		if( $(this).scrollTop() > 2 ) {
 			$('body').addClass('scroll-header');
@@ -9,4 +20,24 @@ $(function() {
 		}
 	});
 
+
+	/*	Text Typing	
+	var i = 0;
+	var txt = document.querySelector(".text-type").html();
+	var speed = 50;
+	function typeWriter() {
+		if (i < txt.length) {
+			document.querySelector(".text-type").innerHTML += txt.charAt(i);
+			i++;
+			setTimeout(typeWriter, speed);
+		}
+	}
+	typeWriter();*/
+
 });
+
+
+
+//Type out examples:
+new TypingText(document.querySelector(".text-type"), 350, function(i){ var ar = new Array(''); return " " + ar[i.length % ar.length]; });
+TypingText.runAll();
